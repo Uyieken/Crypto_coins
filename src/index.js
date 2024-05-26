fc8d6272ab83439ea645d7396cdc3b7c16d46fc0
Henry O. Irabor <93579566+Uyieken@users.noreply.github.com>
2024-05-26 17:41:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from "./App";
-import store from "./app/store";
+import App from './App';
+import store from './app/store';
 
-import "antd/dist/antd.css";
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,5 +16,5 @@ ReactDOM.render(
       </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
